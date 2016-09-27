9a4082c5f5645096f30af2c3d8589e03092d3eb9
yang-xu <yang.xu@linecorp.com>
2016-09-27 18:01:18 +0900

    finish part3

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -12,7 +12,7 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
-        return self.question_text
+        return "%s: %s" % (self.id, self.question_text)
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
@@ -25,4 +25,4 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.choice_text
+        return "%s: %s" % (self.id, self.choice_text)
